9a75964d396780765fe20eb69e6971991aa6c462
Donghyun Hwang <pointehd@naver.com>
2021-02-22 21:59:49 +0900

    useAsync 사용하여 api 연결

--- src/components/BoardBox.js
+++ src/components/BoardBox.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+import useAsync from '../hook/useAsync'
 import Board from './Board';
 
+async function getUsers(){
+    const response = await axios.get(
+        `/board`
+    )
+    console.log(response.data);
+    return response.data.content;
+}
+
 function BoardBox({boards}) {
+    const [state, refetch] = useAsync(getUsers, []);
+    const { loading, data: content, error } = state;
+
+    if(loading) 
+        return <div>로딩중...</div>;
+    if(error) 
+        return <div>error</div>;
+    if(!content) 
+        return null;
     return (
         <ul>
             {process.env.REACT_APP_API_HOST}
-            {boards.map(  board =>
-                <Board key={board.id} id={board.id} content={board.content}/>
+            {content.map( board =>
+                <Board key={board.bid} id={board.bid} content={board.content}/>
                 )}
         </ul>
     );
