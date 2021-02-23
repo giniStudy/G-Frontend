db1a8099380f80661f464f5262a7f41604defdb7
Donghyun Hwang <pointehd@naver.com>
2021-02-24 00:45:58 +0900

    api 호출부분 수정
    Feat. 대수 심

--- src/components/BoardBox.js
+++ src/components/BoardBox.js
@@ -1,33 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import useAsync from '../hook/useAsync'
 import Board from './Board';
+import '../css/Board.css'
 
-async function getUsers(){
+async function getBoards(page){
     const response = await axios.get(
-        `/board`
+        `/board?page=${page}&size=3`
     )
-    console.log(response.data);
     return response.data.content;
 }
 
-function BoardBox({boards}) {
-    const [state, refetch] = useAsync(getUsers, []);
-    const { loading, data: content, error } = state;
+function BoardBox({}) {
+    const [boards, setBoards] = useState([]);
+    const [page, setPage] = useState(0);
+    const [state] = useAsync(() => getBoards(page), [page]);
+    const { loading, data: _boards, error } = state;
+    const fetchBoards =  () => setPage(page + 1);
 
-    if(loading) 
-        return <div>로딩중...</div>;
-    if(error) 
-        return <div>error</div>;
-    if(!content) 
-        return null;
+    useEffect(() => {
+        setBoards([...boards, ...(_boards || [])]);
+    }, [_boards]);
+    
     return (
-        <ul>
-            {process.env.REACT_APP_API_HOST}
-            {content.map( board =>
-                <Board key={board.bid} id={board.bid} content={board.content}/>
-                )}
-        </ul>
+        <section>
+            <div className="page-title">
+                <div className="container">
+                    <h3>게시판</h3>
+                </div>
+            </div>
+            <div className="board-list">
+                <table className="board-table">
+                    <thead>
+                    <tr>
+                        <th scope="col" className="th-num">번호</th>
+                        <th scope="col" className="th-title">제목</th>
+                        <th scope="col" className="th-date">등록일</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        { boards && boards.map( board =><Board key={board.bid} id={board.bid} content={board.content}/>) }
+                        { loading && <tr><td>로딩중...</td></tr> }
+                        { error && <tr><td>error</td></tr> }
+                    </tbody>
+                </table>
+            </div>
+            <button onClick={fetchBoards}>다음</button>
+        </section>
     );
 }
 export default BoardBox;
