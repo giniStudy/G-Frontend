77d1813a0f88cb9d8422ddcb93b0bf3852afb754
Donghyun Hwang <pointehd@naver.com>
2021-02-25 23:12:25 +0900

    들여쓰기변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import '../css/Header.css'
-function Header(){
-    return (
-        <header className="header">
-            <a href="" className="logo">GINI</a>
-            <input className="menu-btn" type="checkbox" id="menu-btn" />
-            <label className="menu-icon"><span className="navicon"></span></label> {/*for="menu-btn"*/}
-            <ul className="menu">
-                <li><a href="#work">Our Work</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#careers">Careers</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </header>
-    )
+import React from "react";
+import "../css/Header.css";
+function Header() {
+  return (
+    <header className="header">
+      <a href="" className="logo">
+        GINI
+      </a>
+      <input className="menu-btn" type="checkbox" id="menu-btn" />
+      <label className="menu-icon">
+        <span className="navicon"></span>
+      </label>{" "}
+      {/*for="menu-btn"*/}
+      <ul className="menu">
+        <li>
+          <a href="#work">Our Work</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#careers">Careers</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
 export default Header;
